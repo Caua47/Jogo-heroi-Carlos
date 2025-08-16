ace13435ea14a1aa6fcf436f343e6e8aea0d4b65
Col3cwz <augustoe436@gmail.com>
2025-08-16 00:46:15 -0300

    versafinal

--- o-heroi-carlos-alfa-2.5/balainimigo.gd
+++ o-heroi-carlos-alfa-2.5/balainimigo.gd
@@ -1,3 +1,23 @@
 extends Area2D
-@export var velocidade = 200
-var direcao = Vector2.ZERO
+@onready var morte = preload("res://tela_de_derrota.tscn")
+var vel = 250
+var dir = -1
+
+func _process(delta: float) -> void:
+	position.x += vel * dir * delta
+	$Sprite2D.flip_h = dir < 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Carlos"):
+		body.queue_free()
+		get_tree().paused = false
+		get_tree().change_scene_to_file("res://tela_de_derrota.tscn")
+	if  body.is_in_group("parede"):
+		queue_free()
+
+
+func morrer():
+	var derrota = morte.instantiate()
+	get_tree().paused = false
+	get_tree().current_scene.add_child(derrota)
